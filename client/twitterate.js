51a39ec8bbbb5dd177e2b16423bf88a0357dd748
fabiomora <fabiomora@gmail.com>
2014-01-20 15:26:01 +0100

    terzo tentativo

--- client/twitterate.js
+++ client/twitterate.js
@@ -1,6 +1,4 @@
-var Twit = require('twit')
-
-var T = new Twit({
+var T = new TWIT({
                 consumer_key: 'GvmS1qpA8JXLL9pRhnSNig',
                 consumer_secret: '31lUbb1LQIfShYDS4ZxhEudBst6nxzkLi98JmFuBo',
                 access_token: '5719602-20bU1bGWPBxbtTUjpnM7Qw7OiRftJfspinZWmRIE',
