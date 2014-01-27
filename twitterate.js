8da2650130f11793e817b1ca563b1e5f95875b8c
physiocoder <ferro.roberto@email.it>
2014-01-27 18:12:32 +0100

    Usati Meteor.methods() e Meteor.call() per dialogo col server e Future per aspettare risposta async di Twitter

--- twitterate.js
+++ twitterate.js
@@ -1,31 +1,38 @@
+if( Meteor.isServer ) {
 
-if( Meteor.isServer) {
-	var T = new Twit({
-                consumer_key: 'GvmS1qpA8JXLL9pRhnSNig',
-                consumer_secret: '31lUbb1LQIfShYDS4ZxhEudBst6nxzkLi98JmFuBo',
-                access_token: '5719602-20bU1bGWPBxbtTUjpnM7Qw7OiRftJfspinZWmRIE',
-                access_token_secret: 'IBgrhJoPm1wR7JWZbvYYDEcFe3qOrsrbLG1n32BreM'
-});
+  var T = new Twit({
+    consumer_key: 'GvmS1qpA8JXLL9pRhnSNig',
+    consumer_secret: '31lUbb1LQIfShYDS4ZxhEudBst6nxzkLi98JmFuBo',
+    access_token: '5719602-20bU1bGWPBxbtTUjpnM7Qw7OiRftJfspinZWmRIE',
+    access_token_secret: 'IBgrhJoPm1wR7JWZbvYYDEcFe3qOrsrbLG1n32BreM'
+  });
 
-T.get( 'search/tweets',
-       { q: '@bravuomo', count: 10 },
-       function(err, reply) { console.log(reply); });				
+  Meteor.methods({
+    elencotweet: function() {
+      var fut = new Future();
+
+      T.get( 'search/tweets', { q: 'grazie', count: 8}, function(err, reply) {
+        console.log("ottenuti i tweet!!!");
+        fut['return'](reply);
+      });
+      
+      return fut.wait();
+    }
+  });
 }
 
-if (Meteor.isClient) {
-	var Tii = new window.Twit({
-                consumer_key: 'GvmS1qpA8JXLL9pRhnSNig',
-                consumer_secret: '31lUbb1LQIfShYDS4ZxhEudBst6nxzkLi98JmFuBo',
-                access_token: '5719602-20bU1bGWPBxbtTUjpnM7Qw7OiRftJfspinZWmRIE',
-                access_token_secret: 'IBgrhJoPm1wR7JWZbvYYDEcFe3qOrsrbLG1n32BreM'
-});
+if( Meteor.isClient ) {
 
-Tii.get( 'search/tweets',
-       { q: '@bravuomo', count: 10 },
-       function(err, reply) { console.log(reply); });				
+  Template.twitterate.helpers({
+    tweets: function() { 
+      if(Session.get('itweets')) return Session.get("itweets").statuses;
+    }
+  });
+
+  Template.twitterate.events = {
+    'click #gettwit': function() {
+      Meteor.call("elencotweet", function(err, argum) {  console.log(argum.statuses); Session.set('itweets', argum); });
+    }
+  };
 }
-
-
-// Template.twitterate.helpers({ tweets: T });
-
 
